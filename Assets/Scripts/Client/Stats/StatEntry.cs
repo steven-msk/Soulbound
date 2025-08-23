f9aae8611df0231855a3ae1437e324aaeae25259
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2025-08-23 17:06:39 +0300

    Set StatEntry type to extend from a stat-source list

--- Assets/Scripts/Client/Stats/StatEntry.cs
+++ Assets/Scripts/Client/Stats/StatEntry.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class StatEntry<TValue> : IStatEntryImpl where TValue : struct, IComparable<TValue> {
-	private readonly List<(SerializableStat<TValue> stat, IStatProvider source)> modifiers = new();
+public class StatEntry<TValue> : List<(SerializableStat<TValue> stat, IStatProvider source)>, IStatEntryImpl
+		where TValue : struct, IComparable<TValue> {
 	public TValue baseValue { get; protected set; }
 	public StatDefinition<TValue> definitionReference { get; protected set; }
 
@@ -13,15 +13,15 @@ public class StatEntry<TValue> : IStatEntryImpl where TValue : struct, IComparab
 	}
 
 	public TValue GetProcessedValue() {
-		return this.definitionReference.processor.ProcessFinalValue(baseValue, modifiers.Select(m => m.stat));
+		return this.definitionReference.processor.ProcessFinalValue(baseValue, this.Select(m => m.stat));
 	}
 
 	public void AddModifier(AbstractSerializableStat serializableStat, IStatProvider source) {
-		this.modifiers.Add(((SerializableStat<TValue>)serializableStat, source));
+		this.Add(((SerializableStat<TValue>)serializableStat, source));
 	}
 
 	public void RemoveModifier(AbstractSerializableStat serializableStat, IStatProvider source) {
-		this.modifiers.Remove(((SerializableStat<TValue>)serializableStat, source));
+		this.Remove(((SerializableStat<TValue>)serializableStat, source));
 	}
 
 	public object GetBoxedValue() => this.GetProcessedValue();
