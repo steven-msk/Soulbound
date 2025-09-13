168f8eef7e3b9e7fd2100967290162a9a9706d5b
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2025-09-13 12:13:56 +0300

    Drop OnActivated and OnDeactivated events from StatActivator because currently there is no use case for them. Might reimplement later on

--- Assets/Scripts/Client/Stats/StatActivator.cs
+++ Assets/Scripts/Client/Stats/StatActivator.cs
@@ -8,14 +8,10 @@ using Unity.VisualScripting;
 #nullable enable
 
 public class StatActivator {
-	public event Action<IStatReceiver>? OnActivated;
-	public event Action<IStatReceiver>? OnDeactivated;
 	public IEnumerable<IStatEffectHandler> effectHandlers { get; private set; }
 
 	public StatActivator(params IStatEffectHandler[] effectHandlers) {
 		this.effectHandlers = effectHandlers;
-		OnActivated += (receiver) => effectHandlers.ToList().ForEach(h => h.Enable(receiver));
-		OnDeactivated += (receiver) => effectHandlers.ToList().ForEach(h => h.Disable(receiver));
 	}
 
 	public StatActivator(Action<Action<IStatReceiver>>? activationBinder, Action<Action<IStatReceiver>>? deactivationBinder, 
@@ -26,10 +22,10 @@ public class StatActivator {
 	}
 
 	public void BeginContext(IStatReceiver receiver) {
-		OnActivated?.Invoke(receiver);
+		effectHandlers.ToList().ForEach(h => h.Enable(receiver));
 	}
 
 	public void EndContext(IStatReceiver receiver) {
-		OnDeactivated?.Invoke(receiver);
+		effectHandlers.ToList().ForEach(h => h.Disable(receiver));
 	}
 }
