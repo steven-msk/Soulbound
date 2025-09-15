3a66cb75c3a782ed1369b3a03eff1bfac8866d0e
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2025-09-15 15:54:36 +0300

    Add item related fields and primitive stat mappings (currently with only 1 stat) to StatItem_test

--- Assets/Scripts/Client/Item/PrototypeTests/StatItem_test.cs
+++ Assets/Scripts/Client/Item/PrototypeTests/StatItem_test.cs
@@ -1,6 +1,7 @@
 ﻿using SoulboundBackend.Client.ItemSystem;
 using SoulboundBackend.Client.Stats;
 using SoulboundBackend.Client.UI.Tooltip;
+using SoulboundBackend.Common;
 using System;
 using System.Collections.Generic;
 
@@ -20,7 +21,23 @@ public class StatItem_test : StatItem {
 	protected override TooltipRenderer.NodeStyleProvider nodeStyleProvider => null;
 
 	public StatItem_test() {
-		this.statMappings = new List<StatMapping>();
-		this.tooltipSupplier = null;
+		StatMappingBuilder mappingBuilder = new StatMappingBuilder()
+			.SetStats(() => new DynamicMap<AbstractSerializableStat>() {
+				["physicalDamage"] = new SerializableStat<int>(StatDefinition.PhysicalDamage, 10, StatApplicationType.Percentage, showAsBonus: true)
+			})
+			.WithTooltipNodes(stats => new List<TooltipNodeData>() {
+				new TooltipNodeData(TooltipNode.Stats, $"While in inventory, gain {stats["physicalDamage"]}.")
+			})
+			.BindEffectHandlers(stats => new DynamicMap<IStatEffectHandler>() {
+				["physicalDamageHandler"] = IStatEffectHandler.Static(this, stats["physicalDamage"])
+			})
+			.BindActivator(handlers => new StatActivator(
+				handlers["physicalDamageHandler"]
+			));
+		this.statMappings = mappingBuilder.ResolveMappings();
+		this.tooltipSupplier = item => new TooltipData.Builder()
+			.AddNode(TooltipNode.Title, this.name)
+			.AddNodes(mappingBuilder.ResolveTooltipNodes())
+			.Finish();
 	}
 }
