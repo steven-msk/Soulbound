e944db8fb4088e86fb6ee60528b4f7afac3f3351
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2025-09-19 15:36:50 +0300

    Begin test framework: implement an actual first test file

--- Assets/Tests/InventoryTests.cs
+++ Assets/Tests/InventoryTests.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
-public class NewTestScript {
-	// A Test behaves as an ordinary method
-	[Test]
-	public void NewTestScriptSimplePasses() {
-		// Use the Assert class to test conditions
-	}
+public class InventoryTests {
 
-	// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-	// `yield return null;` to skip a frame.
 	[UnityTest]
-	public IEnumerator NewTestScriptWithEnumeratorPasses() {
+	public IEnumerator Inventory_InitializesEmptySlots_OnSceneLoad() {
 		SceneManager.LoadScene("SampleScene");
 		yield return null;
 		Assert.IsNotNull(GameManager.instance.Player?.Inventory, "Inventory never initialized");
