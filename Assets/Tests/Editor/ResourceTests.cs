ee22c4fe9bdfeca14281d02760ee519f39a9e167
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2025-09-20 12:10:02 +0300

    Create first editor test: ResourceTests

---
+++ Assets/Tests/Editor/ResourceTests.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using NUnit.Framework;
+using SoulboundBackend.Core.Resource;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+#nullable enable
+
+public class ResourceTests {
+    [Test]
+    public void ResourceManagerLoadsPrefab() {
+        GameObject? prefab = ResourceManager.Get<GameObject, ResourceGroups.Prefabs>("ai test");
+        Assert.That(prefab != null, () => "Failed to load prefab");
+	}
+
+    [UnityTest]
+    public IEnumerator EditorTestScriptWithEnumeratorPasses() {
+        yield return null;
+    }
+}
