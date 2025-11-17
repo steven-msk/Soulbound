e05dd0ecc47b8fda7c7d7089b10f8835a6ea1358
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2025-11-17 16:43:01 +0200

    Install NuGet and NSubstitute

---
+++ Assets/Tests/Editor/InputHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.InputSystem;
+using NSubstitute;
+
+public class InputHandlerTests {
+
+	[SetUp]
+	public void Setup() {
+	}
+}
