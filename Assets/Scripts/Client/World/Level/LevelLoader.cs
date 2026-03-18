00d4166890c47acc405a937d475ab3dc0755f777
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2026-03-18 19:50:41 +0200

    Fixed missing components

--- Assets/Scripts/Client/World/Level/LevelLoader.cs
+++ Assets/Scripts/Client/World/Level/LevelLoader.cs
@@ -19,7 +19,7 @@ namespace SoulboundBackend.Client.World {
 		public async UniTask<WorldSession> LoadLevel(UniTask sceneLoadTask, Func<IWorldSceneRoot> rootProvider) {
 			await sceneLoadTask;
 
-			IWorldSceneRoot sceneRoot = rootProvider();
+			IWorldSceneRoot sceneRoot = rootProvider() ?? throw new OperationException("Root provider returned null");
 
 			Level level = new(seedProvider.GetSeed());
 			LevelManager levelManager = new(level);
