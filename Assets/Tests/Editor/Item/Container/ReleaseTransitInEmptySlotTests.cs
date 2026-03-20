4a6afa7d8f283b0633ab31f2151c958bdc1b0a74
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2026-03-20 20:50:15 +0200

    Added ReleaseTransitInEmptySlotTests

---
+++ Assets/Tests/Editor/Item/Container/ReleaseTransitInEmptySlotTests.cs
@@ -0,0 +1,145 @@
+
+using NSubstitute;
+using NUnit.Framework;
+using SoulboundBackend.Client.ItemSystem;
+using SoulboundBackend.Client.ItemSystem.Container;
+
+namespace ItemTests.Container.Operations {
+	public class ReleaseTransitInEmptySlotTests : SingleSlotOperationTests<ReleaseTransitInEmptySlot> {
+		const int DEFAULT_FULL_STACK = 256;
+		private FakeItem fakeItem;
+
+		[SetUp]
+		public void Setup() {
+			scope = Substitute.For<IItemContainerScope>();
+			fakeItem = new FakeItem {
+				_fullStackSize = DEFAULT_FULL_STACK
+			};
+		}
+
+		protected override ReleaseTransitInEmptySlot GetOperation(IItemContainer container, int slotIndex, IItemContainerScope scope) {
+			return new ReleaseTransitInEmptySlot(container, slotIndex, scope);
+		}
+
+		[Test]
+		public void CanExecute_SlotEmptyAndTransitHasStack_ReturnsTrue() {
+			CreateOperation(fakeItem.CreateStack(), null);
+
+			Assert.That(operation.CanExecute(), Is.True);
+		}
+
+		[Test]
+		public void CanExecute_SlotHasStack_ReturnsFalse() {
+			CreateOperation(null, fakeItem.CreateStack());
+
+			Assert.That(operation.CanExecute(), Is.False);
+		}
+
+		[Test]
+		public void CanExecute_NoTransitStack_ReturnsFalse() {
+			CreateOperation(null, null);
+
+			Assert.That(operation.CanExecute(), Is.False);
+		}
+
+		[Test]
+		public void CanExecute_SlotHasStackAndNoTransit_ReturnsFalse() {
+			CreateOperation(null, fakeItem.CreateStack());
+
+			Assert.That(operation.CanExecute(), Is.False);
+		}
+
+		[Test]
+		public void Execute_ValidConditions_SetsSlotToTransitStack() {
+			ItemStack transitStack = fakeItem.CreateStack();
+			CreateOperation(transitStack, null);
+
+			Assert.That(operation.Execute(), Is.True);
+			Assert.That(slot.HasStack(), Is.True);
+			Assert.That(slot.GetStack(), Is.EqualTo(transitStack));
+
+			slot.Received().SetStack(Arg.Is(transitStack));
+			scope.Received().SetTransitStack(Arg.Is((ItemStack)null));
+		}
+
+		[Test]
+		public void Execute_ValidConditions_ClearsTransitStack() {
+			CreateOperation(fakeItem.CreateStack(), null);
+
+			Assert.That(operation.Execute(), Is.True);
+			Assert.That(scope.HasTransitStack(), Is.False);
+
+			scope.Received().SetTransitStack(Arg.Is((ItemStack)null));
+		}
+
+		[Test]
+		public void Execute_SlotNotEmpty_ReturnsFalse() {
+			CreateOperation(fakeItem.CreateStack(), fakeItem.CreateStack());
+
+			Assert.That(operation.Execute(), Is.False);
+
+			scope.DidNotReceive().SetTransitStack(Arg.Any<ItemStack>());
+			slot.DidNotReceive().SetStack(Arg.Any<ItemStack>());
+		}
+
+		[Test]
+		public void Execute_NoTransitStack_ReturnsFalse() {
+			CreateOperation(null, null);
+
+			Assert.That(operation.Execute(), Is.False);
+
+			scope.DidNotReceive().SetTransitStack(Arg.Any<ItemStack>());
+			slot.DidNotReceive().SetStack(Arg.Any<ItemStack>());
+		}
+
+		[Test]
+		public void Execute_SlotNotEmpty_DoesNotModifySlot() {
+			ItemStack slotStack = fakeItem.CreateStack();
+			CreateOperation(fakeItem.CreateStack(), slotStack);
+
+			Assert.That(operation.Execute(), Is.False);
+			Assert.That(slot.HasStack(), Is.True);
+			Assert.That(slot.GetStack(), Is.EqualTo(slotStack));
+
+			scope.DidNotReceive().SetTransitStack(Arg.Any<ItemStack>());
+			slot.DidNotReceive().SetStack(Arg.Any<ItemStack>());
+		}
+
+		[Test]
+		public void Execute_NoTransitStack_DoesNotModifySlot() {
+			ItemStack slotStack = fakeItem.CreateStack();
+			CreateOperation(null, slotStack);
+
+			Assert.That(operation.Execute(), Is.False);
+			Assert.That(slot.HasStack(), Is.True);
+			Assert.That(slot.GetStack(), Is.EqualTo(slotStack));
+
+			scope.DidNotReceive().SetTransitStack(Arg.Any<ItemStack>());
+			slot.DidNotReceive().SetStack(Arg.Any<ItemStack>());
+		}
+
+		[Test]
+		public void Execute_SlotNotEmpty_DoesNotModifyTransitStack() {
+			ItemStack transitStack = fakeItem.CreateStack();
+			CreateOperation(transitStack, fakeItem.CreateStack());
+
+			Assert.That(operation.Execute(), Is.False);
+			Assert.That(scope.HasTransitStack(), Is.True);
+			Assert.That(scope.GetTransitStack(), Is.EqualTo(transitStack));
+
+			scope.DidNotReceive().SetTransitStack(Arg.Any<ItemStack>());
+			slot.DidNotReceive().SetStack(Arg.Any<ItemStack>());
+		}
+
+		[Test]
+		public void Execute_NoTransitStack_DoesNotModifyTransitStack() {
+			CreateOperation(null, null);
+
+			Assert.That(operation.Execute(), Is.False);
+			Assert.That(scope.HasTransitStack(), Is.False);
+
+			scope.DidNotReceive().SetTransitStack(Arg.Any<ItemStack>());
+			slot.DidNotReceive().SetStack(Arg.Any<ItemStack>());
+		}
+	}
+}
