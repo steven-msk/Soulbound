b1067e75a85bda4223cd5016523a216933153958
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2026-05-15 16:12:04 +0300

    re-added WorldScreen UI

--- Assets/Game/SoulboundEngine/Client/UI/Screen/WorldScreen.cs
+++ Assets/Game/SoulboundEngine/Client/UI/Screen/WorldScreen.cs
@@ -1,34 +1,38 @@
-using SoulboundEngine.Client.ItemSystem.Container;
-using SoulboundEngine.Client.ItemSystem.Container.View;
 using SoulboundEngine.Client.Players;
 using SoulboundEngine.Client.Render.Item;
+using SoulboundEngine.Core.Assets;
+using UnityEngine.UIElements;
 
 namespace SoulboundEngine.Client.UI.Screen {
-	public sealed class WorldScreen : Screen {
+	public sealed class WorldScreen : UxmlScreen {
 		private readonly Player player;
 		private readonly ItemRenderManager itemRenderManager;
 
-		public WorldScreen(ItemRenderManager itemRenderManager, Player player) {
+		public WorldScreen(ItemRenderManager itemRenderManager, Player player) 
+			: base(AssetManager.Resolve<VisualTreeAsset>(new AssetKey("WorldScreen"))) {
 			this.itemRenderManager = itemRenderManager;
 			this.player = player;
 		}
 
 		public override bool ReturnWithEscape => false;
 
+		protected override void OnBind(VisualElement root) {
+		}
+
 		//public override IScreenObject BuildObject(IScreenObjectFactory objFactory) {
 		//	return base.BuildObject(new WorldSessionScreenFactory(this.itemRenderManager, objFactory));
 		//}
 
-		protected override void OnBuild(IScreenHandle handle) {
-			this.player.SetTransitStackSource((ITransitStackSource)handle);
+		//protected override void OnBuild(IScreenHandle handle) {
+		//	this.player.SetTransitStackSource((ITransitStackSource)handle);
 
-			PlayerInventoryRenderer inventoryUIBuilder = new(this.itemRenderManager, this.player.GetInventory());
-			inventoryUIBuilder.Build(
-				(IItemContainerScreenScope)handle,
-				out IItemContainerHandle inventory,
-				out IItemContainerHandle hotbar
-			);
-			inventoryUIBuilder.FixInventoryPosition(inventory, hotbar);
-		}
+		//	PlayerInventoryRenderer inventoryUIBuilder = new(this.itemRenderManager, this.player.GetInventory());
+		//	inventoryUIBuilder.Build(
+		//		(IItemContainerScreenScope)handle,
+		//		out IItemContainerHandle inventory,
+		//		out IItemContainerHandle hotbar
+		//	);
+		//	inventoryUIBuilder.FixInventoryPosition(inventory, hotbar);
+		//}
 	}
 }
