8641bbcf539ed638f10af0147607d679897a5f36
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2026-08-04 16:26:52 +0300

    added test SignBlock and SignTileEntity implementations

--- Assets/Game/SoulboundEngine/Client/World/Block/Blocks/SignBlock.cs
+++ Assets/Game/SoulboundEngine/Client/World/Block/Blocks/SignBlock.cs
@@ -1,20 +1,33 @@
 ﻿using SoulboundEngine.Client.Debug.Logging;
+using SoulboundEngine.Client.Interaction;
 using SoulboundEngine.Client.Item;
 using SoulboundEngine.Client.Player;
+using SoulboundEngine.Client.World.Block.Entity;
 using SoulboundEngine.Client.World.Block.State;
 
 namespace SoulboundEngine.Client.World.Block {
-	public class SignBlock : Block {
+	using Level = Level.Level;
+
+	public class SignBlock : Block, ITileEntityProvider {
 		public SignBlock(Settings settings) 
 			: base(settings) {
 		}
 
-		protected override void OnHoverEnter(BlockState state, ItemStack stack, Level.Level level, PlayerEntity player, BlockPos pos) {
+		public TileEntity CreateTileEntity(BlockPos pos, BlockState state) {
+			return new SignTileEntity(pos, state);
+		}
+
+		protected override void OnHoverEnter(BlockState state, ItemStack stack, Level level, PlayerEntity player, BlockPos pos) {
 			Logger.LogInfo("sign hover enter: {}", pos);
 		}
 
-		protected override void OnHoverLeave(BlockState state, ItemStack stack, Level.Level level, PlayerEntity player, BlockPos pos) {
+		protected override void OnHoverLeave(BlockState state, ItemStack stack, Level level, PlayerEntity player, BlockPos pos) {
 			Logger.LogInfo("sign hover leave: {}", pos);
+		}
+
+		protected override IActionResult OnSecondaryUse(BlockState state, Level level, PlayerEntity player, BlockPos pos) {
+			Logger.LogInfo("sign set text triggered");
+			return IActionResult.SUCCESS;
 		}
 	}
 }
---
+++ Assets/Game/SoulboundEngine/Client/World/Block/TileEntities/SignTileEntity.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using SoulboundEngine.Client.World.Block.State;
+
+namespace SoulboundEngine.Client.World.Block.Entity {
+	public class SignTileEntity : TileEntity {
+		private string text;
+
+		private SignTileEntity(TileEntityType tileEntityType, BlockPos blockPos, BlockState blockState) 
+			: base(tileEntityType, blockPos, blockState) {
+		}
+
+		public SignTileEntity(BlockPos blockPos, BlockState state)
+			: this(TileEntityType.SIGN, blockPos, state) {
+		}
+
+		public static SignTileEntity Create(BlockPos blockPos, BlockState state) {
+			return new SignTileEntity(blockPos, state);
+		}
+
+		public override void Read(JToken json) {
+			this.text = (string)json["text"];
+		}
+
+		public override void Write(JObject json) {
+			json["text"] = this.text;
+		}
+
+		public string GetText() => this.text;
+	}
+}
