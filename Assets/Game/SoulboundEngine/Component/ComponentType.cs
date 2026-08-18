c98128e205309ef675b2267d0664ab676b8cf83d
Miskolczi Stefan <stefan.miskolczi2017@gmail.com>
2026-08-18 21:15:12 +0300

    added ComponentChanges json serialization

--- Assets/Game/SoulboundEngine/Component/ComponentType.cs
+++ Assets/Game/SoulboundEngine/Component/ComponentType.cs
@@ -1,23 +1,48 @@
 ﻿namespace SoulboundEngine.Component {
 	using Newtonsoft.Json.Linq;
+	using SoulboundEngine.Registry;
 	using System;
+	using System.Collections.Generic;
 
 #nullable enable
 
 	public abstract class ComponentType {
+		public static readonly Dictionary<Identifier, ComponentType> FROM_ID = new();
+		private readonly RegistryKey<ComponentType> key;
+
+		protected ComponentType(RegistryKey<ComponentType> key) {
+			this.key = key;
+			FROM_ID.Add(key.value, this);
+		}
+
+		public abstract bool IsTransient();
+
+		public abstract JToken ToJson(object value);
+
+		public abstract object FromJson(JToken token);
+
+		public RegistryKey<ComponentType> GetKey() => this.key;
+
+		public static ComponentType? Get(Identifier id) {
+			return FROM_ID.GetValueOrDefault(id);
+		}
+
+		public static Identifier GetId(ComponentType type) {
+			return type.key.value;
+		}
+
 		public sealed record Codec<T>(Func<T, JToken> encoder, Func<JToken, T> decoder) {
 			public JToken Encode(T value) => this.encoder(value);
 
 			public T Decode(JToken token) => this.decoder(token);
 		}
-
-		public abstract bool IsTransient();
 	}
 
 	public class ComponentType<T> : ComponentType {
 		private readonly Codec<T>? codec;
 
-		private ComponentType(Codec<T>? codec) {
+		private ComponentType(RegistryKey<ComponentType> key, Codec<T>? codec) 
+			: base(key) {
 			this.codec = codec;
 		}
 
@@ -31,10 +56,24 @@
 			return this.codec == null;
 		}
 
-		public static Builder Create() => new();
+		public override JToken ToJson(object value) {
+			if (value is not T typed) throw new InvalidCastException($"Component value {value} cannot be cast to {typeof(T)}");
+			return this.CodecOrThrow(new NotSupportedException($"Cannot serialize transient component '{GetId(this)}'")).Encode(typed);
+		}
+
+		public override object FromJson(JToken token) {
+			return this.CodecOrThrow(new NotSupportedException($"Cannot serialize transient component '{GetId(this)}'")).Decode(token);
+		}
+
+		public static Builder Create(RegistryKey<ComponentType> key) => new(key);
 
 		public sealed class Builder {
+			private readonly RegistryKey<ComponentType> key;
 			private Codec<T>? codec;
+
+			public Builder(RegistryKey<ComponentType> key) {
+				this.key = key;
+			}
 
 			public Builder Codec(Codec<T> codec) {
 				this.codec = codec;
@@ -42,7 +81,7 @@
 			}
 
 			public ComponentType<T> Build() {
-				return new ComponentType<T>(this.codec);
+				return new ComponentType<T>(this.key, this.codec);
 			}
 		}
 	}
--- Assets/Game/SoulboundEngine/Item/ItemComponents.cs
+++ Assets/Game/SoulboundEngine/Item/ItemComponents.cs
@@ -4,7 +4,12 @@
 	using SoulboundEngine.Registry;
 	using System;
 
+#nullable enable
+
 	public static class ItemComponents {
+		public static readonly ComponentType.Codec<int> INT_CODEC = GenericValueCodec(token => (int)token);
+		public static readonly ComponentType.Codec<string> STRING_CODEC = GenericValueCodec(token => (string)token);
+
 		public static readonly ComponentType<string> NAME = Register<string>("name", b => b.Codec(STRING_CODEC));
 		public static readonly ComponentType<int> MAX_STACK_COUNT = Register<int>("max_stack_count", b => b.Codec(INT_CODEC));
 		public static readonly ComponentType<int> BREAK_LEVEL = Register<int>("break_level", b => b.Codec(INT_CODEC));
@@ -14,15 +19,12 @@
 			.Add(MAX_STACK_COUNT, Item.DEFAULT_FULL_STACK)
 			.Build();
 
-		public static readonly ComponentType.Codec<int> INT_CODEC = GenericValueCodec(token => (int)token);
-		public static readonly ComponentType.Codec<string> STRING_CODEC = GenericValueCodec(token => (string)token);
-
 		private static ComponentType.Codec<T> GenericValueCodec<T>(Func<JToken, T> caster) => new(value => new JValue(value), caster);
 
 		private static ComponentType<T> Register<T>(string id, Func<ComponentType<T>.Builder, ComponentType<T>.Builder> builder) {
-			ComponentType<T> componentType = builder(ComponentType<T>.Create()).Build();
-			Registry<ComponentType>.Register(Registries.COMPONENT_TYPE, id, componentType);
-			return componentType;
+			RegistryKey<ComponentType> key = RegistryKey<ComponentType>.Of(Registries.COMPONENT_TYPE.GetKey(), Identifier.Of(id));
+			ComponentType<T> componentType = builder(ComponentType<T>.Create(key)).Build();
+			return Registry<ComponentType>.Register(Registries.COMPONENT_TYPE, key, componentType);
 		}
 	}
 }
